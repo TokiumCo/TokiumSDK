b4053aebc319e5355152181a99462de2579cd8d0
amy-mavis-qin <amyqin501@gmail.com>
2022-11-16 22:27:45 -0500

    Update baseURL

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const tokiumAPI = axios.create({
-    baseURL: 'https://c9bqm649d0.execute-api.us-east-2.amazonaws.com/'
+    baseURL: 'https://api.tokium.co/'
 });
 
 export default tokiumAPI;
